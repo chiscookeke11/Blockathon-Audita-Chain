4bd99c72e92fd0136234f2d843bff634944e0e44
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-10-31 14:25:13 +0100

    [fix: app routing]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,63 +1,78 @@
-import React from 'react'
-import './footer.css'
-import { FaInstagram, FaWhatsapp, FaYoutube } from 'react-icons/fa'
-import { FaLinkedin, FaTwitter } from 'react-icons/fa6'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./footer.css";
+import { FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import { FaLinkedin, FaTwitter } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-
-
-        <div>
-            <h1>AuditaBlock</h1>
-            <p>AuditaBlock Streamlines auditing processes, enhancing efficency, transparency and security</p>
-            <div className="icons">
-                <a href="#"><FaWhatsapp/> </a>
-                <a href="#"><FaInstagram/>   </a>
-                <a href="#"><FaTwitter/> </a>
-                <a href="#"><FaLinkedin/> </a>
-                <a href="#"><FaYoutube/> </a>
-            </div>
+    <div className="footer">
+      <div>
+        <h1>AuditaBlock</h1>
+        <p>
+          AuditaBlock Streamlines auditing processes, enhancing efficency,
+          transparency and security
+        </p>
+        <div className="icons">
+          <a href="#">
+            <FaWhatsapp />{" "}
+          </a>
+          <a href="#">
+            <FaInstagram />{" "}
+          </a>
+          <a href="#">
+            <FaTwitter />{" "}
+          </a>
+          <a href="#">
+            <FaLinkedin />{" "}
+          </a>
+          <a href="#">
+            <FaYoutube />{" "}
+          </a>
         </div>
+      </div>
 
-
-        <div>
-           <ul>
-
-            <h3>Product</h3>
-            <li>Benefits</li>
-            <li>Security</li>
-            <li>Affiliates</li>
-            <li>E-commerce</li>
-           </ul>
-        </div>
-
-        <div>
+      <div>
         <ul>
-            <h3>Navigation</h3>
-           <NavLink to={"/project_management"} ><li>Home</li></NavLink>
-           <NavLink to={"/applayout"}> <li>About Us</li></NavLink>
-           <NavLink to={"/smartcontractregistry"}> <li>Blog</li></NavLink>
-            <li>Contact Us</li>
-           </ul>          
-        </div>
+          <h3>Product</h3>
+          <li>Benefits</li>
+          <li>Security</li>
+          <li>Affiliates</li>
+          <li>E-commerce</li>
+        </ul>
+      </div>
 
-
-        <div>
-          
+      <div>
         <ul>
-            <h3>Company</h3>
-            <li>About</li>
-            <li>Commerce</li>
-            <li>Courses</li>
-            <li>Contact Us</li>
-            <li>Knowledge</li>
-            <li>Demo</li>
-           </ul> 
-        </div>
+          <h3>Navigation</h3>
+          <NavLink to={"/project_management"}>
+            <li>Home</li>
+          </NavLink>
+          <NavLink to={"/app"}>
+            {" "}
+            <li>About Us</li>
+          </NavLink>
+          <NavLink to={"/smartcontractregistry"}>
+            {" "}
+            <li>Blog</li>
+          </NavLink>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <h3>Company</h3>
+          <li>About</li>
+          <li>Commerce</li>
+          <li>Courses</li>
+          <li>Contact Us</li>
+          <li>Knowledge</li>
+          <li>Demo</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
