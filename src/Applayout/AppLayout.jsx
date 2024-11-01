9a3cf9c51dbd5570def48982c352b2e0828d8bb2
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-11-01 10:01:31 +0100

    [feat: sidenav]

--- src/Applayout/AppLayout.jsx
+++ src/Applayout/AppLayout.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidenav from "../components/sidenav/Sidenav";
 import Header from "../components/header/Header";
 import "./appLayout.css";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
+  const [isOpen, setIsOpen] = useState();
+
   return (
     <div className="applayout">
-      <Sidenav />
+      <Sidenav isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="info-container">
-        <Header />
+        <Header setIsOpen={setIsOpen} />
         <Outlet />
       </div>
     </div>
--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import './sideNav.css'
-const Sidenav = () => {
+import { useState } from "react";
+import "./sideNav.css";
+import { NavLink } from "react-router-dom";
+import { FiX } from "react-icons/fi";
+const Sidenav = ({ isOpen, setIsOpen }) => {
+  const handleItemClick = () => {
+    setIsOpen(false);
+  };
   return (
-    <div className='sidenav'>
+    <div
+      className={`sidenav block lg:w-[23%] max-md:fixed max-md:left-0 max-md:top-0 max-md:w-[80%] max-md:h-full max-md:transition-transform max-md:duration-300 transform z-50 ${
+        isOpen ? "max-md:translate-x-0" : "max-md:-translate-x-full"
+      } z-50`}
+    >
+      <FiX
+        size={30}
+        onClick={() => setIsOpen(false)}
+        className="hidden max-md:flex absolute top-3 right-3"
+      />
       <h1>Logo</h1>
       <ul>
-        
-        <li>Dashboard</li>
-        <li>Transaction</li>
+        <NavLink to="dashboard" onClick={handleItemClick}>
+          <li>Dashboard</li>
+        </NavLink>
+        <NavLink to="transactions" onClick={handleItemClick}>
+          <li>Transaction</li>
+        </NavLink>
         <li>Contacts</li>
         <li>Settings</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidenav
+export default Sidenav;
