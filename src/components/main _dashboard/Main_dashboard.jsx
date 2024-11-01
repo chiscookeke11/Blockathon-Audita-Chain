7d74f60a5434f1307cee753b1c8a54ea65f5720a
Elite-tch <elite6461@gmail.com>
2024-11-01 13:08:07 +0100

    Your commit message

--- src/components/main _dashboard/Main_dashboard.jsx
+++ src/components/main _dashboard/Main_dashboard.jsx
@@ -1,61 +1,15 @@
 import React from 'react'
+import { FaUser } from 'react-icons/fa'
 
 export default function Main_dashboard() {
   return (
     <div>
-    <div className='grid md:grid-cols-2  gap-8 mt-10'>
-<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
-    <div>
-    <h3 className='text-gray-400'>spent this month</h3>
-    <h1 className='font-bold text-2xl pt-1'>$682.5</h1>
-</div>
-<img src="/chart.jpg" alt="" className='w-14' /> 
-</div>
-
-<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
-    <div className='flex items-center gap-4'>
-<div className='bg-blue-700 px-4 py-4 rounded-full'>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 text-white">
-  <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
-</svg>
-
-</div>
-
-<div>
-    <h3 className='text-gray-400'>New clients</h3>
-    <h1 className='font-bold text-2xl pt-1'>321</h1>
-</div>
-    </div>
-<img src="/chart2.jpg" alt="" className='w-14' /> 
- 
-</div>
     
-    
-    
-<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
-    <div className='flex items-center gap-3'>
-<img src="/chart.jpg" alt="" className='w-12 bg-blue-300 px-3 py-3 rounded-full' />
-<div>
-    <h3 className='text-gray-400'>Earnings</h3>
-    <h1 className='font-bold text-2xl pt-1'>$382.5</h1>
-</div>
-    </div>
-<img src="/chart2.jpg" alt="" className='w-12' /> 
-</div>
-
-<div className='bg-blue-700 mx-6 px-8 py-4 rounded-lg flex  justify-between'>
-    <div>
-    <h3 className='text-gray-300'>Activity</h3>
-    <h1 className='font-bold text-2xl pt-1 text-gray-100'>$540.50</h1>
-</div>
-<img src="/chart3.jpg" alt="" className='w-12' /> 
-</div>
-</div>  
     
 <div className='grid lg:grid-cols-3 gap-5 md:mt-16 md:grid-cols-2  mx-6'>
     <div className='bg-gray-100  px-6 py-6 rounded-lg'>
 <div className='flex flex-col items-center gap-3'>
-<img src="" alt="" className='w-28 h-28 rounded-full '/>
+<div className='w-28 h-28 rounded-full user flex items-center justify-center'><FaUser className='h-16 w-16'/> </div>
 <h1 className='font-bold text-2xl'>Charles Robbie</h1>
 <div className='flex items-center gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -161,6 +115,56 @@ export default function Main_dashboard() {
 
     </div>
 
+
+
+    <div className='grid md:grid-cols-2  gap-8 mt-10'>
+<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
+    <div>
+    <h3 className='text-gray-400'>spent this month</h3>
+    <h1 className='font-bold text-2xl pt-1'>$682.5</h1>
+</div>
+<img src="/chart.jpg" alt="" className='w-14' /> 
+</div>
+
+<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
+    <div className='flex items-center gap-4'>
+<div className='bg-blue-700 px-4 py-4 rounded-full'>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 text-white">
+  <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+</svg>
+
+</div>
+
+<div>
+    <h3 className='text-gray-400'>New clients</h3>
+    <h1 className='font-bold text-2xl pt-1'>321</h1>
+</div>
+    </div>
+<img src="/chart2.jpg" alt="" className='w-14' /> 
+ 
+</div>
+    
+    
+    
+<div className='bg-white mx-6 px-8 py-4 rounded-lg flex  justify-between'>
+    <div className='flex items-center gap-3'>
+<img src="/chart.jpg" alt="" className='w-12 bg-blue-300 px-3 py-3 rounded-full' />
+<div>
+    <h3 className='text-gray-400'>Earnings</h3>
+    <h1 className='font-bold text-2xl pt-1'>$382.5</h1>
+</div>
+    </div>
+<img src="/chart2.jpg" alt="" className='w-12' /> 
+</div>
+
+<div className='bg-blue-700 mx-6 px-8 py-4 rounded-lg flex  justify-between'>
+    <div>
+    <h3 className='text-gray-300'>Activity</h3>
+    <h1 className='font-bold text-2xl pt-1 text-gray-100'>$540.50</h1>
+</div>
+<img src="/chart3.jpg" alt="" className='w-12' /> 
+</div>
+</div>  
     
     
         </div>
