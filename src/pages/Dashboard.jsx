ba9e60ed94fbfdca425d96b8ef57fa3781dc5501
Elite-tch <elite6461@gmail.com>
2024-11-01 12:31:51 +0100

    Your commit message

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import IdCard from "../components/id-card/IdCard";
 import Stats from "../components/stats/Stats";
-
+import Main_dashboard from "../components/main _dashboard/Main_dashboard";
 const DashboardMain = () => {
   return (
     <div style={{ overflow: "auto", height: "100vh" }}>
+<Main_dashboard />
       <IdCard />
       <Stats />
     </div>
